26a58ea65fd4b9726dfe8a73cfb5bae632f9114d
小◕黑◕贝 <346972930@qq.com>
2018-11-04 17:30:39 +0800

    vueload

---
+++ examples/button.vue
@@ -0,0 +1,29 @@
+//button.vue
+<template>
+    <div class="button-warp">
+        <button class="button">{{text}}</button>
+    </div>
+</template>
+<script>
+module.exports = {
+  data: function() {
+    return {
+      text: "world"
+    };
+  }
+//   ,
+//   components: {
+//     "my-button": vueload("./button.vue", "my-button")
+//   }
+};
+</script>
+<style scoped>
+    .button-warp{
+        display:inline-block;
+    }
+    .button{
+        padding: 5px 10px;
+        font-size: 12px;
+        border-radus: 2px;
+    }
+</style>
---
+++ examples/my-component.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="hello">Hello {{who}}
+        <Button></Button>
+    </div>
+</template>
+
+<script>
+// Vue.component('login',vueload("./login.vue"));
+module.exports = {
+  data: function() {
+    return {
+      who: "world"
+    };
+  },
+  components: {
+    "Button": vueload("./button.vue", "Button")
+    
+  }
+};
+</script>
+
+<style>
+.hello {
+  background-color: #ffe;
+}
+</style>
